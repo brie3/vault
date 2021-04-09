f0b2bface16b235cc85c4d64786e8150f95dfe8f
brie3 <b3r6i9@gmail.com>
2021-04-09 14:00:42 +0500

    app config for vault ready

--- cmd/main.go
+++ cmd/main.go
@@ -10,11 +10,35 @@ import (
 
 func main() {
 	vaultAddr := os.Getenv("VAULT_ADDR")
-	vaultToken := os.Getenv("VAULT_TOKEN")
 	dbSecretPath := os.Getenv("DB_SECRET_PATH")
 
-	fmt.Printf("addr: %s, token: %s, path: %s\n", vaultAddr, vaultToken, dbSecretPath)
+	config := &vault.Config{Address: vaultAddr, Timeout: 20 * time.Second}
 
-	config := vault.Config{Timeout: 5 * time.Second}
-	fmt.Println(config.Address)
+	vaultClient, err := vault.NewClient(config)
+	if err != nil {
+		fmt.Println("can't create a new vault client instance: %w", err)
+		return
+	}
+
+	var ok bool
+	var dbURL map[string]interface{}
+	for attempt := time.Duration(1); attempt < 6; attempt++ {
+		dbSecret, err := vaultClient.Logical().Read(dbSecretPath)
+		if err != nil {
+			fmt.Printf("can't read secret from vault: %v", err)
+			return
+		}
+
+		if dbURL, ok = dbSecret.Data["data"].(map[string]interface{}); ok {
+			break
+		}
+		fmt.Printf("attempt #%d - can't get data from vault client: %v\n", attempt, dbSecret)
+		time.Sleep(attempt * time.Second)
+	}
+
+	if !ok {
+		fmt.Printf("can't find db_url key in secret")
+	} else {
+		fmt.Printf("secret from vault is: %v", dbURL["db_url"])
+	}
 }
